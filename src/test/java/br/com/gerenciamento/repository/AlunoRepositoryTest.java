8297605d5904fb9983cc7c78f5eb4bc08d219ee3
GENILDOBURGOS <gnldburgos@gmail.com>
2025-06-29 17:27:58 -0300

    test(repository): cobertura inicial de testes unitários para AlunoRepositoryTest e UsuarioRepositoryTest

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,114 @@
 package br.com.gerenciamento.repository;
 
-import org.junit.runner.RunWith;
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Test
+    public void deveRetornarQuantidadeDeAlunosAtivos() {
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Jose Mateus");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.ADMINISTRACAO);
+        aluno1.setStatus(Status.ATIVO);
+        aluno1.setMatricula("123456");
+        alunoRepository.save(aluno1);
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Vinicius");
+        aluno2.setTurno(Turno.MATUTINO);
+        aluno2.setCurso(Curso.BIOMEDICINA);
+        aluno2.setStatus(Status.ATIVO);
+        aluno2.setMatricula("4444");
+        alunoRepository.save(aluno2);
+
+        assertEquals(2, alunoRepository.findByStatusAtivo().size());
+    }
+
+    @Test
+    public void deveRetornarQuantidadeDeAlunosInativos() {
+        Aluno alunoInativo = new Aluno();
+        alunoInativo.setNome("Ribeiro");
+        alunoInativo.setTurno(Turno.MATUTINO);
+        alunoInativo.setCurso(Curso.DIREITO);
+        alunoInativo.setStatus(Status.INATIVO);
+        alunoInativo.setMatricula("11111");
+        alunoRepository.save(alunoInativo);
+
+        assertEquals(1, alunoRepository.findByStatusInativo().size());
+    }
+
+    @Test
+    public void deveLancarExcecaoParaAlunoComNomeCurto() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Ana");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+
+        ConstraintViolationException ex = assertThrows(ConstraintViolationException.class, () -> {
+            alunoRepository.save(aluno);
+        });
+
+        assertTrue(
+                ex.getConstraintViolations().stream()
+                        .anyMatch(v -> v.getPropertyPath().toString().equals("nome"))
+        );
+    }
+
+    @Test
+    public void deveEncontrarAlunosPorParteDoNome() {
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Americo Luis");
+        aluno1.setTurno(Turno.NOTURNO);
+        aluno1.setCurso(Curso.INFORMATICA);
+        aluno1.setStatus(Status.INATIVO);
+        aluno1.setMatricula("091232");
+        alunoRepository.save(aluno1);
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Americo Jose");
+        aluno2.setTurno(Turno.NOTURNO);
+        aluno2.setCurso(Curso.INFORMATICA);
+        aluno2.setStatus(Status.INATIVO);
+        aluno2.setMatricula("091233");
+        alunoRepository.save(aluno2);
+
+        assertEquals(2, alunoRepository.findByNomeContainingIgnoreCase("americo").size());
+    }
+
+    @Test
+    public void deveLancarExcecaoParaAlunoSemMatricula() {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Jorge");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.INFORMATICA);
+        aluno.setStatus(Status.ATIVO);
+
+        ConstraintViolationException ex = assertThrows(ConstraintViolationException.class, () -> {
+            alunoRepository.save(aluno);
+        });
+
+        assertTrue(
+                ex.getConstraintViolations().stream()
+                        .anyMatch(v -> v.getPropertyPath().toString().equals("matricula"))
+        );
+    }
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,71 @@
 package br.com.gerenciamento.repository;
 
-import org.junit.runner.RunWith;
+import br.com.gerenciamento.model.Usuario;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void deveLancarExcecaoParaUsuarioComNomeMuitoCurto() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("a1"); // menos de 3 caracteres
+        usuario.setSenha("123");
+        usuario.setEmail("alex@gmail.com");
+
+        ConstraintViolationException ex = assertThrows(ConstraintViolationException.class, () -> {
+            usuarioRepository.save(usuario);
+        });
+
+        assertTrue(
+                ex.getConstraintViolations().stream()
+                        .anyMatch(v -> v.getPropertyPath().toString().equals("user"))
+        );
+    }
+
+    @Test
+    public void deveEncontrarUsuarioPorEmail() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("Epitacio");
+        usuario.setSenha("123");
+        usuario.setEmail("taco@gmail.com");
+        usuarioRepository.save(usuario);
+
+        Usuario usuarioEncontrado = usuarioRepository.findByEmail("taco@gmail.com");
+
+        assertNotNull(usuarioEncontrado);
+        assertEquals(usuario.getUser(), usuarioEncontrado.getUser());
+    }
+
+    @Test
+    public void deveBuscarUsuarioPorLogin() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("User");
+        usuario.setSenha("senha");
+        usuario.setEmail("usuario@gmail.com");
+        usuarioRepository.save(usuario);
+
+        Usuario usuarioLogin = usuarioRepository.buscarLogin("User", "senha");
+
+        assertNotNull(usuarioLogin);
+        assertEquals(usuario.getUser(), usuarioLogin.getUser());
+        assertEquals(usuario.getSenha(), usuarioLogin.getSenha());
+    }
+
+    @Test
+    public void deveRetornarNuloQuandoUsuarioNaoExiste() {
+        Usuario usuario = usuarioRepository.buscarLogin("alex", "123");
+        assertNull(usuario);
+    }
 }
